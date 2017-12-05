5de5759fc8c4883edb2ce3fd4670626ebbdf367a
Jason Vance <jbvance@gmail.com>
2017-12-05 19:01:31 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,4 +1,12 @@
 class Dog
+
+  attr_accessor :name, :breed
+  attr_reader :id
+
+  def initialize(id=nil, name, breed)
+    @id, @name, @breed = id, name, breed
+  end
+
   def self.create_table
     sql = <<-SQL
       CREATE TABLE IF NOT EXISTS dogs (
